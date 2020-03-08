52de905cc611147d267d8dc63bb6d5925f97d58b
= <ashif8984@gmail.com>
2020-03-09 01:44:24 +0530

    Ordered the Folder Structure

---
+++ Ansible/Installation/AnsibleInstall.sh
@@ -0,0 +1,11 @@
+# Ansible_Master
+
+sudo apt-get update
+sudo apt-get install software-properties-common
+sudo apt-add-repository ppa:ansible/ansible $ sudo apt-get update
+sudo apt-get install ansible
+
+# Install Python
+
+sudo apt-get update
+sudo apt-get install python
